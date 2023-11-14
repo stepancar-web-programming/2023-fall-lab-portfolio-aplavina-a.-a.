b58d9c0289d6d77635d1d33fd64c2826bd822207
anna <annaaplavina@gmail.com>
2023-11-14 23:20:07 +0300

    added github link to skills page

--- src/pages/skills/Skills.js
+++ src/pages/skills/Skills.js
@@ -1,16 +1,10 @@
 import './skills.css'
+
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
-
-import arrow from './../../img/arrow.png'
-
-import dockerSkill from './../../img/skills/docker.png';
-import javaSkill from './../../img/skills/java.png';
-import springSkill from './../../img/skills/spring.png';
-import gitSkill from './../../img/skills/git.png';
-import nodeSkill from './../../img/skills/node.png';
-import postgresSkill from './../../img/skills/postgres.svg';
 import Slider from '../../components/slider/Slider';
+
+import githubIcon from './../../img/icons/github.png';
 
 const Skills = () => {
     return (
@@ -20,6 +14,10 @@ const Skills = () => {
                 <div className='container'>
                     <h2 className="skills-main-title">My skills</h2>
                     <Slider/>
+                    <a className='github-link' href="https://github.com/AnnaAplavina">
+                        <img src={githubIcon}/>
+                        <p>GitHub</p>
+                    </a>
                 </div>
             </main>
             <Footer/>
